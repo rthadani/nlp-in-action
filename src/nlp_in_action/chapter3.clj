24068ce83840ff8a0226f17a9fe5c72bcdc68834
Rohit Thadani <rthadani@plume.com>
2021-01-09 17:11:42 -0800

    LEftover

--- src/nlp_in_action/chapter3.clj
+++ src/nlp_in_action/chapter3.clj
@@ -1,2 +1,19 @@
 (ns nlp-in-action.chapter3)
 
+
+(def build-one-row
+  [document-tf columns]
+  )
+
+(defn build-doc-tf
+  [document tokenizer]
+  (let [frequency-word-builder (comp frequencies u/remove-stop-words tokenizer)]
+    (->> (frequency-word-builder document)
+        (map (fn [word frequency] [word (/ frequency (count document))]))
+        (into {}))))
+
+
+(defn build-word-vector
+  [document-tfs]
+  (let [columns (reduce (fn [s tf] (into s (keys tf))) document-tfs)]
+    ))
